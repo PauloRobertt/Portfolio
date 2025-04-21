1c15a48a8c12f850ed4c2dd6fd62873ae7715371
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-04-21 15:01:46 -0300

    refactor(front-end): convertendo estrutura estática em HTML/CSS para aplicação em React

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import Header from './components/Header'
+
+export default function App() {
+
+  return (
+    <div className='containerApp'>
+      <Header/>
+    </div>
+  )
+}
+
