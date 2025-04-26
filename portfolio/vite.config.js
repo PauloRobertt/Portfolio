a05fb863c80d79cb3be806e5e349a271762dd8b0
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-04-26 20:57:32 -0300

    fix

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     open: true,
   },
-  base: process.env.VITE_BASE_PATH || "/Portfolio"
 })
