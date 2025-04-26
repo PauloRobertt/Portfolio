c495eeaa558a5ddcc76330fed351aa72e58d67c6
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-04-25 22:05:12 -0300

    chore(assets): organiza imagens dos projetos e adiciona novas versões em .jpeg
    - Adicionado arquivo OrganizarImages.js para centralizar as importações
    - Substituída imagem da sorveteria (.png) por versão otimizada (.jpeg)
    - Adicionadas imagens dos projetos Costs e Restaurante

---
+++ portfolio/src/assets/OrganizarImages.js
@@ -0,0 +1,15 @@
+import bannerSorveteria from './img/imgProjetos/banner-Sorveteria.jpeg';
+import bannerCosts from './img/imgProjetos/banner-Costs.jpeg';
+import bannerAgencia from './img/imgProjetos/bannerAgencia.png';
+import bannerRestaurante from './img/imgProjetos/banner-Restaurante.jpeg';
+import bannerControleEstoque from './img/imgProjetos/controle-estoque.png';
+import FundoDesenvolvimento from './img/imgProjetos/FundoDesenvolvimento.jpg';
+
+export const objImagens = {
+    projectCosts: bannerCosts,
+    projectSorveteria: bannerSorveteria,
+    projectAgencia: bannerAgencia,
+    projectRestaurante: bannerRestaurante,
+    projectControleEstoque: bannerControleEstoque,
+    fundoDesenvolvimento: FundoDesenvolvimento
+}
